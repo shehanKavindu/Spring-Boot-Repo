f8edff6e9a52dc0bc1782a2d58ab44a61692b585
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-09-17 20:50:21 +0530

    add new class

--- src/main/java/lk/ijse/spring/Appinitializer.java
+++ src/main/java/lk/ijse/spring/Appinitializer.java
@@ -2,6 +2,7 @@ package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
 import lk.ijse.spring.pojo.Skpojo;
+import lk.ijse.spring.pojo.Userpojo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
@@ -15,6 +16,9 @@ public class Appinitializer {
         System.out.println(bean);
         Skpojo skpojo = new Skpojo();
         skpojo.play();
+        Userpojo userpojo = new Userpojo();
+        userpojo.dance();
+
     }
 }
 
---
+++ src/main/java/lk/ijse/spring/pojo/Userpojo.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.pojo;
+import org.springframework.stereotype.Component;
+@Component
+
+public class Userpojo {
+    public Userpojo(){
+        System.out.println("fgf");
+    }
+    public void dance(){
+        System.out.println("dance");
+    }
+}
